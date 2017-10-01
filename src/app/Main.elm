0b6d9509bad77a8afd6917c5c94bd29701a44ba5
Andrew Ellis <ellis@hhd.com.au>
2017-10-01 13:46:01 +1100

    home page design

--- src/app/Main.elm
+++ src/app/Main.elm
@@ -1,10 +1,11 @@
 module Main exposing (..)
 
-import Html exposing (Html, div, h1, h2, img, button, label, text)
+import Html exposing (Html, div, h1, h2, img, button, label, span, text)
 import Html.Attributes exposing (class, src, style, width, height)
 import Html.Events exposing (onClick)
 
 -- TODO: Dynamically generate the subheading text
+-- TODO: Dynamically generate button text (innovate, disrupt, synergise, go viral)
 main =
   Html.beginnerProgram { model = subheading, view = view, update = update }
 
@@ -30,7 +31,7 @@ update msg subheading =
 
 view : Subheading -> Html Ipsum
 view subheading =
-  div [class "wrapper", style [("background-image", "url('images/background-6.jpg')")]]
+  div [class "wrapper", style [("background-image", "url('images/background-1.jpg')")]]
     [ div [class "overlay"] []
     , div [class "container"]
         [ div [class "logo"]
@@ -39,8 +40,41 @@ view subheading =
         ]
         , h2 [class "subheading"] [text subheading]
         , div [class "form"]
-        [ label [class "form__label"] [text "I want 3 paragraphs between 5 and 7 sentences long"]
-          , button [ class "button", onClick GenerateIpsum ] [text "New ipsum"]
-        ]
-      ]
+          [ div [class "form__row"]
+            [ label [class "form__label"] [text "I want"]
+            , div [class "form__input-wrap"]
+              [ span [class "form__input"] [text "3"]
+              , div [class "form__toggle"]
+              [ span [class "form__toggle-up"] []
+              , span [class "form__toggle-down"] []
+              ]
+            ]
+            , label [class "form__label"] [text "paragraphs"]
+            ]
+          , div [class "form__row"]
+            [ label [class "form__label"] [text "with at least"]
+            , div [class "form__input-wrap"]
+              [ span [class "form__input"] [text "5"]
+              , div [class "form__toggle"]
+              [ span [class "form__toggle-up"] []
+              , span [class "form__toggle-down"] []
+              ]
+            ]
+            , label [class "form__label"] [text "sentences"]
+            ]
+          , div [class "form__row"]
+            [ label [class "form__label"] [text "and no more than"]
+            , div [class "form__input-wrap"]
+              [ span [class "form__input"] [text "7"]
+              , div [class "form__toggle"]
+              [ span [class "form__toggle-up"] []
+              , span [class "form__toggle-down"] []
+              ]
+            ]
+            , label [class "form__label"] [text "."]
+            ]
+          , div [class "form__row"]
+            [ button [ class "button form__button", onClick GenerateIpsum ] [text "Surprise and delight"] ]
+          ]
+       ]
     ]
